2b79b949cb45ee66f857bfc187d3ee5890566c23
Zielak <zielak69@gmail.com>
2014-01-02 15:27:32 +0100

    added new lines

--- config.js
+++ config.js
@@ -1,15 +1,16 @@
 var config = {}
 
 
-/* How does antiFlood work here?
- User's new messages will only be accepted when his timer is below `border`.
- You can use either static or incremental anti flood.
- 'static' - user's timer will be set to a `timer` value on each accepted message.
- 'incremental' - will add `timer` value on each accepted message.
- User cannot speak when his timer is above `border` value.
- All time units are in miliseconds, which means you can make more
- accurate antiFlood protection (USE WITH CAUTION)
- */
+/*
+  How does antiFlood work here?
+  User's new messages will only be accepted when his timer is below `border`.
+  You can use either static or incremental anti flood.
+  'static' - user's timer will be set to a `timer` value on each accepted message.
+  'incremental' - will add `timer` value on each accepted message.
+  User cannot speak when his timer is above `border` value.
+  All time units are in miliseconds, which means you can make more
+  accurate antiFlood protection (USE WITH CAUTION)
+*/
 config.antiFlood = {
   type: 'incremental',
   timer: 500,
@@ -18,6 +19,10 @@ config.antiFlood = {
 
 config.moderation = {
   active: true
+}
+
+config.mongo = {
+  link: "mongodb://localhost:27017/chatDb"
 }
 
 config.user = {
@@ -39,7 +44,7 @@ config.message = {
   }
 }
 
-// chatColor is also defined in CSS files
+// chatColor should also defined in CSS files
 config.groups = {
   "guest": {},
   "mod": {
